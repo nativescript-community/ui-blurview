4bd62d7c04db4198624fd5a4df12497a3098e8c6
Martin Guillon <martin.guillon@akylas.fr>
2018-07-15 17:41:36 +0200

    fix

--- plugin/src/blurview.common.d.ts
+++ plugin/src/blurview.common.d.ts
@@ -1,5 +1,5 @@
-import { View } from 'tns-core-modules/ui/core/view';
-export declare const blurRadiusProperty: any;
+import { View, Property } from 'tns-core-modules/ui/core/view';
+export declare const blurRadiusProperty: Property<BlurView, number>;
 export declare class BlurView extends View {
     constructor();
     blurRadius: number;
--- plugin/src/blurview.common.ts
+++ plugin/src/blurview.common.ts
@@ -1,16 +1,14 @@
 import { View, Property } from 'tns-core-modules/ui/core/view';
 
 export const blurRadiusProperty = new Property<BlurView, number>({
-    name: 'bluRadius',
+    name: 'blurRadius',
+    defaultValue: 16,
     valueConverter: v => parseFloat(v)
 });
 export class BlurView extends View {
     constructor() {
         super();
     }
-    public blurRadius: number = 16;
-    [blurRadiusProperty.getDefault](): number {
-        return this.blurRadius;
-    }
+    public blurRadius: number;
 }
 blurRadiusProperty.register(BlurView);
