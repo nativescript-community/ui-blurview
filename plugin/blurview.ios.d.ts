60435f1ef1d4a70cd220967d4bd02840a8450267
Martin Guillon <martin.guillon@akylas.fr>
2019-09-19 11:43:25 +0200

    chore: cleanup

--- plugin/blurview.ios.d.ts
+++
@@ -1,7 +0,0 @@
-import { BlurViewBase } from './blurview.common';
-export declare class BlurView extends BlurViewBase {
-    nativeView: VisualEffectView;
-    blurEffect: UIBlurEffect;
-    constructor();
-    createNativeView(): UIVisualEffectView;
-}
